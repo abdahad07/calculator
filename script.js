e342f9117c3a17358eb4cc5f958dcaf744e97d8d
Abdul Ahad <ammaar07@gmail.com>
2026-04-01 23:10:25 +0530

    simplify math operator function

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-// Math functions
-
-const add = (a, b) => a + b;
-
-const subtract = (a, b) => a - b;
-
-const multiply = (a, b) => a * b;
-
-const divide = (a, b) => a / b;
-
 // Operate function
-
 const operate = (a, b, operator) => {
   switch (operator) {
     case "+":
-      return add(a, b);
+      return a + b;
     case "-":
-      return subtract(a, b);
+      return a - b;
     case "*":
-      return multiply(a, b);
+      return a * b;
     case "/":
-      return divide(a, b);
+      return a / b;
   }
 };
